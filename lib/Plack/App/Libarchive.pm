a89306cab5226c2206d0daabeb4e70bfe8d1d812
Graham Ollis <plicease@cpan.org>
2022-08-31 01:10:07 -0600

    Initial structure

---
+++ lib/Plack/App/Libarchive.pm
@@ -0,0 +1,12 @@
+package Plack::App::Libarchive;
+
+use strict;
+use warnings;
+use 5.008004;
+
+# ABSTRACT: Serve an archive via libarchive as a PSGI web app
+# VERSION
+
+1;
+
+
---
+++ t/plack_app_libarchive.t
@@ -0,0 +1,8 @@
+use Test2::V0 -no_srand => 1;
+use Plack::App::Libarchive;
+
+ok 1, 'todo';
+
+done_testing;
+
+
